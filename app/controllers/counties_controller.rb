46d11fb903f681439739e418ef8a1ad219b3c3d5
Fabio Martins <fabio@inventae.com.br>
2013-11-25 17:11:51 -0200

    Upcase string

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -2,7 +2,7 @@ class CountiesController < ApplicationController
   respond_to :json
 
   def index
-    @county = County.where( name: params[:code] ).first
+    @county = County.where( name: params[:code].upcase ).first
     respond_with( @county )
   end
 end
