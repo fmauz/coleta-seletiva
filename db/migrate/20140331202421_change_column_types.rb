f3a7e64efbfdf227408ba33c4bc4cdf24b1c345a
Fabio Martins <fabio@inventae.com.br>
2014-03-31 17:42:04 -0300

    Casting

--- db/migrate/20140331202421_change_column_types.rb
+++ db/migrate/20140331202421_change_column_types.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypes < ActiveRecord::Migration
   def change
-    change_column :cards, :year, 'integer USING CAST(column_name AS integer)'
-    change_column :cards, :month, 'integer USING CAST(column_name AS integer)'
+    change_column :cards, :year, 'integer USING CAST(year AS integer)'
+    change_column :cards, :month, 'integer USING CAST(month AS integer)'
   end
 end
